6005f6c844ceb0405eaedf005b26bc76d62b29cc
Dmitry <dmitrygrechkin04@gmail.com>
2023-07-02 15:04:08 +0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "./components/Header"
+import NavigationBar from "./components/NavigationBar"
+import ScrollBar from "./components/ScrollBar"
+import PageHeader from "./components/PageHeader"
+import About from "./components/About"
+import TeamMembers from "./components/TeamMembers"
+import AboutVideo from "./components/AboutVideo"
+import Footer from "./components/Footer"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (<div className="name">
+    <Header />
+    <NavigationBar />
+    <PageHeader />
+    <About />
+    <TeamMembers />
+    <AboutVideo />
+    <Footer />
+
+
+  </div>)
+  }
+
+  inputClick() {console.log("clicked")}
+  mouseOver() {console.log("Mouse over")}
 }
-
-export default App;
+export default App
